6967ff7bfeb8264f62b9f1416a4cdecd49c05f2e
Deynuh <60407390+Deynuh@users.noreply.github.com>
2024-12-26 22:37:07 -0800

    vc

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import AddingForm from '../components/AddingForm.vue'
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <!-- <TheWelcome /> -->
+    <AddingForm />
   </main>
 </template>
