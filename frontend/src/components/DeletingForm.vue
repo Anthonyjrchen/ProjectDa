2dcb651afbbdf29ad577396564cbbaa38f22faf4
Deynuh <60407390+Deynuh@users.noreply.github.com>
2025-01-01 16:56:32 -0800

    delete form updates

--- frontend/src/components/DeletingForm.vue
+++ frontend/src/components/DeletingForm.vue
@@ -5,7 +5,13 @@ import { ref } from 'vue';
 <template>
     <head>
         <link href="./output.css" rel="stylesheet">
-    </head>    
+    </head> 
+
+    <h2>Court File No.</h2>
+    <input type="text" name="courtFile" v-model="courtFile" class="p-1.5 !border-[1px] !border-dark-white text-black !rounded-md focus:outline-none focus:ring-1 focus:ring-white" required />
+        
+    <button class="border-[1px] border-dark-white px-3 py-1.5 rounded-md hover:bg-dark-white mt-3" type="submit">Delete</button>
+    
 </template>
 
 <style scoped>
