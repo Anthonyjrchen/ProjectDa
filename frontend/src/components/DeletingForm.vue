9557b5e309916856080920017d86f132b42efd9d
Deynuh <60407390+Deynuh@users.noreply.github.com>
2025-01-01 17:07:25 -0800

    routed delete to add

--- frontend/src/components/DeletingForm.vue
+++ frontend/src/components/DeletingForm.vue
@@ -6,12 +6,19 @@ import { ref } from 'vue';
     <head>
         <link href="./output.css" rel="stylesheet">
     </head> 
+    <div class="p-1.5">
+        <h2>Court File No.</h2>
+        <input type="text" name="courtFile" v-model="courtFile" class="p-1.5 !border-[1px] !border-dark-white text-black !rounded-md focus:outline-none focus:ring-1 focus:ring-white" required />
+            
+        <button class="border-[1px] border-dark-white px-3 py-1.5 rounded-md hover:bg-dark-white mt-3" type="submit">Delete</button>
+    </div>
 
-    <h2>Court File No.</h2>
-    <input type="text" name="courtFile" v-model="courtFile" class="p-1.5 !border-[1px] !border-dark-white text-black !rounded-md focus:outline-none focus:ring-1 focus:ring-white" required />
-        
-    <button class="border-[1px] border-dark-white px-3 py-1.5 rounded-md hover:bg-dark-white mt-3" type="submit">Delete</button>
-    
+    <div class="p-0.5">
+        <router-link to="/home">
+            <button class="border-[1px] border-dark-white px-3 py-1.5 rounded-md hover:bg-dark-white mt-3" type="submit">Go to Add</button>
+        </router-link>
+    </div>
+
 </template>
 
 <style scoped>
--- frontend/src/views/DeleteView.vue
+++ frontend/src/views/DeleteView.vue
@@ -6,25 +6,12 @@ import DeletingForm from '@/components/DeletingForm.vue';
 
 <template>
   <main>
-    <div class="container">
       <div class="deletingForm">
         <DeletingForm />
       </div>
-
-    </div>
   </main>
 </template>
 
 <style scoped>
-
-.container {
-  display: flex; /* Flexbox for side-by-side layout */
-  gap: 16px; /* Add spacing between the divs */
-  box-sizing: border-box; /* Ensures padding is included in width calculations */
-}
-
-* {
-  box-sizing: border-box; /* Ensure padding doesn't affect overall width */
-}
 
 </style>
